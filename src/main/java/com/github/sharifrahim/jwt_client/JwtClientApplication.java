5eab42654b49b6b4de7c881181f49998c1c6f929
sharifrahim <sharifrahim92@gmail.com>
2025-06-25 04:18:37 +0800

    initial commit

---
+++ src/main/java/com/github/sharifrahim/jwt_client/JwtClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.sharifrahim.jwt_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sharifrahim/jwt_client/JwtClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sharifrahim.jwt_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
